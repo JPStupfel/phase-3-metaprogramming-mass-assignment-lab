e6c1392c6ee773752f2f61b1ca92915f7a618a64
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-06-23 13:51:03 -0500

    commit tree 8006d5b3d10624a232ce08d155314ee4e3301cbd

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
+require 'pry'
 class Person
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+  :weight, :handed, :complexion, :t_shirt_size,
+  :wrist_size, :glove_size, :pant_length, :pant_width
+
+
   # your code here
+
+  def initialize attributes
+    attributes.each do |key, value|
+    self.send "#{key}=", value
+    end
+  end
+
+
 end
+
+bob_attributes = { name: "Bob", hair_color: "Brown" }
+bob = Person.new(bob_attributes)
+binding.pry
